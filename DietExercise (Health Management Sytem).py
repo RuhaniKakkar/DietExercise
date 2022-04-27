d437b596e0b009e215e465daec9f16873ebf8648
RuhaniKakkar <ruhanikrishankakkar2002@gmail.com>
2022-04-27 18:22:08 +0530

    final commit

---
+++ DietExercise (Health Management Sytem).py
@@ -0,0 +1,51 @@
+# DietExercise (Health Management Sytem)
+client_list = {1: "Samir", 2: "Rohit", 3: "Asaf"}
+log_list = {1: "Exercise", 2: "Diet"}
+
+
+def getdate():
+    import datetime
+    return datetime.datetime.now()
+
+
+try:
+    print("Select client`s name:")
+    for key, value in client_list.items():
+        print("Press", key, "for", value, "\n", end="")
+    client_name = int(input())
+
+    print("You selected Client : ", client_list[client_name], "\n", end="")
+
+    print("Press 1 for Log")
+    print("Press 2 for Retrieve")
+    ch = int(input())
+
+    if ch==1:
+        for key, value in log_list.items():
+            print("Press", key, "to log", value, "\n", end="")
+        log_name = int(input())
+        print("Selected Job : ", log_list[log_name])
+        f = open(client_list[client_name] + "_" + log_list[log_name] + ".txt", "a")
+        k = 'y'
+        while (k!="n"):
+            print("Enter", log_list[log_name], "\n", end="")
+            mytext = input()
+            f.write("[ " + str(getdate()).replace(":", "-") + " ] : " + mytext + "\n")
+            k = input("ADD MORE ? y/n:")
+            continue
+        f.close()
+    elif ch == 2:
+        for key, value in log_list.items():
+            print("Press", key, "to retrieve", value, "\n", end="")
+        log_name = int(input())
+        print(client_list[client_name], "-", log_list[log_name], "Report :", "\n", end="")
+        f = open(client_list[client_name] + "_" + log_list[log_name] + ".txt", "rt")
+        contents = f.readlines()
+        for line in contents:
+            print(line, end="")
+        f.close()
+    else:
+        print("Input is Invalid! Sorry.")
+except Exception as e:
+    print("INPUT IS WRONG!")
+
